c2243934d94939cd8f500176e2cdc2ec23894faa
rohitnayal12 <rohitnayalkld@gmail.com>
2023-07-20 22:33:24 +0530

    day-3

--- upstyle/src/Redux/productReducer/productReducer.ts
+++ upstyle/src/Redux/productReducer/productReducer.ts
@@ -1,5 +1,6 @@
 
 import { ProductType } from "../../constants";
+import { GET_PRODUCT_MEN_FAILURE, GET_PRODUCT_MEN_REQUEST, GET_PRODUCT_MEN_SUCCESS, GET_PRODUCT_WOMEN_FAILURE, GET_PRODUCT_WOMEN_REQUEST, GET_PRODUCT_WOMEN_SUCCESS } from "../actionTypes";
 
 type AuthAction = {
     type: string;
@@ -28,16 +29,42 @@ type AuthAction = {
   
   export const productReducer = (state: AuthState = initialState, action: AuthAction) => {
     switch (action.type) {
-      case "SET_LOADING":
+      case GET_PRODUCT_MEN_REQUEST:
         return {
           ...state,
           isLoading: true,
         };
-      case "SET_ERROR":
+        case GET_PRODUCT_MEN_FAILURE:
+          return {
+            ...state,
+            isLoading:false,
+            isError:true,
+          }
+        case GET_PRODUCT_MEN_SUCCESS:
         return {
           ...state,
-          isError: true,
+        isLoading:false,
+        mens:action.payload
         };
+
+        case GET_PRODUCT_WOMEN_REQUEST:
+          return {
+            ...state,
+            isLoading: true,
+          };
+          case GET_PRODUCT_WOMEN_FAILURE:
+            return {
+              ...state,
+              isLoading:false,
+              isError:true,
+            }
+          case GET_PRODUCT_WOMEN_SUCCESS:
+          return {
+            ...state,
+          isLoading:false,
+          womens:action.payload
+          };
+        
       
       default:
         return state;
